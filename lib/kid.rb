2d0c920933445a0f4cb16b3c1e79c198ad2001b3
wei chen <weichen9083@gmail.com>
2020-02-08 19:43:09 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,9 +1,11 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './'
 
 class Kid 
-  include Dance 
-  extend MetaDancing
+ include FancyDance::InstanceMethods 
+ extend FancyDance::ClassMethods 
+  
   
   attr_accessor :name 
   
